116255703bb812a701eaa86d3f7f8d25323a1079
NarmetaAlekya <146024293+NarmetaAlekya@users.noreply.github.com>
2024-08-09 21:56:51 +0530

    Update week1_3johnson.py

--- week1_3johnson.py
+++ week1_3johnson.py
@@ -1,20 +1,17 @@
-import numpy as np
-print("Enter the number of orders:")
-n=int(input())
 print("Enter orders Priority Number:")
-def orders(priority_num):
-    for i in range(1, len(priority_num)):
-        key = priority_num[i]
+n=int(input())
+def priority_numbers(l):
+    for i in range(1, len(l)):
+        key = l[i]
         j = i - 1
-        while j >= 0 and key < priority_num[j]:
-            priority_num[j + 1] = priority_num[j]
+        while j >= 0 and key < l[j]:
+            l[j + 1] = l[j]
             j -= 1
-        priority_num[j + 1] = key
-    return priority_num
+        l[j + 1] = key
+    return l
 l=[]
 for i in range(0,n):
   m=int(input())
   l.append(m)
-priority_num=np.array(l)
-print(orders(priority_num))
 
+print(priority_numbers(l))
