f065ae6a484c114217d1e41beb17a5308e108dc4
NarmetaAlekya <146024293+NarmetaAlekya@users.noreply.github.com>
2024-08-09 21:44:59 +0530

    Update week1_3Nelema.py

--- week1_3Nelema.py
+++ week1_3Nelema.py
@@ -1,18 +1,17 @@
-import numpy as np
 print("Enter orders Priority Number:")
 n=int(input())
-def priority_numbers(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
+def priority_numbers(l):
+    for i in range(1, len(l)):
+        key = l[i]
         j = i - 1
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
+        while j >= 0 and key < l[j]:
+            l[j + 1] = l[j]
             j -= 1
-        arr[j + 1] = key
-    return arr
+        l[j + 1] = key
+    return l
 l=[]
 for i in range(0,n):
   m=int(input())
   l.append(m)
-arr=np.array(l)
-print(priority_numbers(arr))
+
+print(priority_numbers(l))
