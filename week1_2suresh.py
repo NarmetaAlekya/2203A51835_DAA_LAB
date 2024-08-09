d4dcd9f36b05c5a56f335d547dc2921fae14106a
NarmetaAlekya <146024293+NarmetaAlekya@users.noreply.github.com>
2024-08-09 21:51:17 +0530

    Update week1_2suresh.py

--- week1_2suresh.py
+++ week1_2suresh.py
@@ -1,20 +1,16 @@
-import numpy as np
-print("Enter the price of the products:")
-n=int(input())
-def products_order(price):
-    for i in range(1, len(price)):
-        key = price[i]
-        j = i - 1
-        while j >= 0 and key < price[j]:
-            price[j + 1] = price[j]
-            j -= 1
-        price[j + 1] = key
-    return price
-l=[]
-for i in range(0,n):
-  m=int(input())
-  l.append(m)
-price=np.array(l)
-print("prices in order:");
-print(products_order(price))
+l = []
+def delivery_package():
+  for i in range(0,len(l)-1):
+      for j in range(0,len(l)-1):
+          if(l[j]>l[j+1]):
+              temp=l[j]
+              l[j]=l[j+1]
+              l[j+1]=temp
+  return l
 
+n=int(input())
+print("enter the tracking numbers")
+for i in range(0, n):
+    m = int(input())
+    l.append(m)
+print(delivery_package())
